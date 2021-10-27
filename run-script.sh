de16785fe03e7a4df7661df110f096477e61527c
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2021-10-27 15:01:22 +0200

    Riktig path til env-filen.

--- run-script.sh
+++ run-script.sh
@@ -3,7 +3,7 @@ echo "Running run-script.sh"
 
 if [ -z "$INNLOGGINGSINFO_API_URL" ]; then
   echo "INNLOGGINGSINFO_API_URL is not set, exporting"
-  export $(var/run/secrets/nais.io/vault/innloggingsinfo_api.env | xargs)
+  export $(/var/run/secrets/nais.io/vault/innloggingsinfo_api.env | xargs)
 else
   echo "INNLOGGINGSINFO_API_URL already set"
 fi
