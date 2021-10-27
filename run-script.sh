6685776fe2316f9049e9326da91f73456e357fd5
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2021-10-27 14:36:42 +0200

    Setter miljøvariabel manuelt da den ikke satt globalt.

--- run-script.sh
+++ run-script.sh
@@ -1,39 +1,14 @@
 #!/bin/bash
 echo "Running run-script.sh"
 
-#Associative array 
-declare -A variables
-variables[INNLOGGINGSINFO_API_URL]=$INNLOGGINGSINFO_API_URL
-
-forEachVariable() {
-  for variable in "${!variables[@]}"
-    do
-      ($1 $variable) || exit $?
-    done
-}
-
-checkAvailability () {
-  if test -z ${variables[$1]}; then
-    echo "For å kunne starte applikasjonen må ${1} være satt."
-    echo "Avbryter oppstart."
-    exit 1
-  fi
-}
-
-printAvailability () {
-	echo "* ${1}"
-}
-
-makeAvailable() {
-  echo "window.env.${1}=\"${variables[$1]}\";" >> ../build/config.js
-}
-
-(forEachVariable checkAvailability) || exit $?
-
-echo "Tilgjengeliggjør følgende miljøvariabler for frontend-en:"
-forEachVariable printAvailability
+if [ -z "$INNLOGGINGSINFO_API_URL" ]; then
+  echo "INNLOGGINGSINFO_API_URL is not set, exporting"
+  export $(var/run/secrets/nais.io/vault/innloggingsinfo_api.env | xargs)
+else
+  echo "INNLOGGINGSINFO_API_URL already set"
+fi
 
 echo "window.env={};" > ../build/config.js
-forEachVariable makeAvailable
+echo "window.env.INNLOGGINGSINFO_API_URL=\"$INNLOGGINGSINFO_API_URL\";" >> ../build/config.js
 
 node ./server.js
